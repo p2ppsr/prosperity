75320ed9a0f2d8456dffde028edba715dbd07bf1
Brayden Langley <brayden167@gmail.com>
2024-02-13 10:55:37 -0800

    webpack should support esm by default

--- babel.config.js
+++ babel.config.js
@@ -2,20 +2,20 @@ const plugins = [
   [
     'babel-plugin-import',
     {
-      'libraryName': '@mui/material',
+      libraryName: '@material-ui/core',
       // Use "'libraryDirectory': ''," if your bundler does not support ES modules
-      'libraryDirectory': 'esm',
-      'camel2DashComponentName': false
+      libraryDirectory: '',
+      camel2DashComponentName: false
     },
     'core'
   ],
   [
     'babel-plugin-import',
     {
-      'libraryName': '@mui/icons-material',
+      libraryName: '@material-ui/icons',
       // Use "'libraryDirectory': ''," if your bundler does not support ES modules
-      'libraryDirectory': 'esm',
-      'camel2DashComponentName': false
+      libraryDirectory: '',
+      camel2DashComponentName: false
     },
     'icons'
   ]
@@ -23,5 +23,5 @@ const plugins = [
 
 module.exports = {
   plugins,
-  presets: ["@babel/preset-env", "@babel/preset-react"]
+  presets: ['@babel/preset-env', '@babel/preset-react']
 }
